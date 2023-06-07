580e0f3edac07528a5815f5889bdf6abc8bc1bc8
paresiqbal <thecrowdel@gmail.com>
2023-06-07 21:21:07 +0700

    Add env

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
-export default function Home() {
+const getGames = async () => {
+  const response = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.RAWG}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export default async function Home() {
+  const games = await getGames();
+  console.log(games);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
